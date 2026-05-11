f567091d8d181ea3d5191250824f99240e632afd
Alvaro Perez <alva.perezz@duocuc.cl>
2026-05-10 20:09:42 -0400

    Se modifica para visualizar par mostrar el pipeline de nuevo

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // esto se agrega para testear
-
+// hola
 const {
   DB_HOST = "10.0.158.183", // acá colocar la IP Privada EC2 DB
   DB_USER = "alumno",
